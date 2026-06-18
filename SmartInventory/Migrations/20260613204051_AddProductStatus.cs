6d7a97c9838fdba4aea2291e4fa9f8de7c3b045c
AngeloTargaryen <mauriciosaavedrayoung@gmail.com>
2026-06-18 18:59:27 -0300

    feat: agregar estados de productos

---
+++ SmartInventory/Migrations/20260613204051_AddProductStatus.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartInventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AvailableQuantity",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderedQuantity",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoldQuantity",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderedQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SoldQuantity",
+                table: "Products");
+        }
+    }
+}
--- SmartInventory/Models/Product.cs
+++ SmartInventory/Models/Product.cs
@@ -9,9 +9,20 @@ namespace SmartInventory.Models;
 public class Product
 {
     public int Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
+
     public string Description { get; set; } = string.Empty;
+
     public decimal Price { get; set; }
+
     public int Stock { get; set; }
+
     public string Category { get; set; } = string.Empty;
+
+    public int AvailableQuantity { get; set; }
+
+    public int SoldQuantity { get; set; }
+
+    public int OrderedQuantity { get; set; }
 }
